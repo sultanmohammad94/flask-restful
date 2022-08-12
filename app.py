8d99d58d1767f82604d0df939bccd9c2fc8f1fea
MHD KHAIR SULTAN <sultanmohammad94@gmail.com>
2022-08-12 22:06:14 +0300

    change the fake database into a real database, and create a serializer for Task class

--- app.py
+++ app.py
@@ -1,44 +1,73 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, marshal_with, fields
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+DATABASE_NAME = 'flask_dev.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DATABASE_NAME}"
+db = SQLAlchemy(app)
+# Database
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=True)
+    
+    def __repr__(self) -> str:
+        return self.name
+
+
+#We use marshal as django serializer
+#here we list all the files we want to see
+tasks_fields = {
+    'id':fields.Integer,
+    'name':fields.String,
+}
+#REST API
 api = Api(app)
 
-fake_data = {
-    1:{'name':'clean car'},
-    2:{'name':'write blog'},
-    3:{'name':'buy food'},
-    4:{'name':'build portfolio'},
-}
-
 class Items(Resource):
-    # get all items
+    
+    @marshal_with(tasks_fields)
     def get(self):
-        return fake_data
-    # Add new item
+        tasks = Task.query.all()
+        return tasks
+    
+    @marshal_with(tasks_fields)
     def post(self):
         data = request.json
-        item_id = len(fake_data.keys()) + 1
-        fake_data[item_id] = {'name':data['name']}
-        return fake_data
-    
+        task = Task(name=data['name'])
+        db.session.add(task)
+        db.session.commit()
+        tasks = Task.query.all()
+        return tasks
+
 class Item(Resource):
-    # get a single item
+    
+    @marshal_with(tasks_fields)
     def get(self, pk):
-        return fake_data.get(pk)
-    # update item
+        task = Task.query.filter_by(id=pk).first()
+        return task
+    
+    @marshal_with(tasks_fields)
     def put(self, pk):
         data = request.json
-        fake_data[pk]['name'] = data['name']
-        return fake_data
+        task = Task.query.filter_by(id=pk).first()
+        task.name = data['name']
+        db.session.commit()
+        return task
     
-    # delete item
+    
+    @marshal_with(tasks_fields)
     def delete(self, pk):
-        del fake_data[pk]
-        return fake_data
-
+        task = Task.query.filter_by(id=pk).first()
+        db.session.delete(task)
+        db.session.commit()
+        tasks = Task.query.all()
+        return tasks
+    
+#REST-API Routing
 api.add_resource(Items,'/')
 api.add_resource(Item,'/<int:pk>')
 
+#Main App
 if __name__ == "__main__":
     app.run(debug=True)
